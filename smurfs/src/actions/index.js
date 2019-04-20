4ea53b177df8e79bef23887a926377f07ebe0589
unknown <kcmp711@gmail.com>
2019-04-20 12:18:46 -0400

    created getSmurfs and addSmurf actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -13,3 +13,35 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+import axios from 'axios';
+export const GET_SMURFS = 'GET_SMURFS';
+export const GET_SMURFS_SUCCESS = 'GET_SMURFS_SUCCESS';
+export const GET_SMURFS_FAILURE = 'GET_SMURFS_FAILURE';
+export const ADD_SMURF = 'ADD_SMURF';
+export const ADD_SMURF_SUCCESS = 'ADD_SMURF_SUCCESS';
+export const ADD_SMURF_FAILURE = 'ADD_SMURF_FAILURE';
+
+
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: GET_SMURFS });
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(res => {
+      dispatch({ type: GET_SMURFS_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: GET_SMURFS_FAILURE, payload: err });
+    });
+};
+export const addSmurf = ({ name, age, height }) => dispatch => {
+  dispatch({ type: ADD_SMURF });
+  axios
+    .post('http://localhost:3333/smurfs', { name, age, height })
+    .then(res => {
+      dispatch({ type: ADD_SMURF_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: ADD_SMURF_FAILURE, payload: err });
+    });
+};
